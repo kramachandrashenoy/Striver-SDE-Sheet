f3785b10af03b008222ddd35dfd7c8938e11e710
K Ramachandra Shenoy <117439389+kramachandrashenoy@users.noreply.github.com>
2024-04-04 12:53:29 +0530

    Create Remove Duplicates from Sorted Array.py

---
+++ Day 7 Linked List and Arrays/Remove Duplicates from Sorted Array.py
@@ -0,0 +1,28 @@
+26. Remove Duplicates from Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once.
+The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Solution:
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i,j=0,1
+        while i<=j and j<len(nums):
+            if nums[i]==nums[j]:
+                j+=1
+            else:
+                nums[i+1]=nums[j]
+                i+=1
+        return i+1
+
+
+        
+
